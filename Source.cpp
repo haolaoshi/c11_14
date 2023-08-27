1f8f87820246868ad41dd0a49e4ccb95f06753b7
haolaoshi <liujiahao.oak@gmail.com>
2023-08-27 23:09:12 +0800

    09 =default ,=delete

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 
+/*01 - variadic template*/
 void print()
 {
 
@@ -26,6 +27,41 @@ void print(const T& firstArg, const Types&... args)
 	cout << firstArg << " " << n << endl;
 	print(args...);
 }
+/*01 - end*/
+
+/*07 - explicit for ctors taking more than one argu*/
+struct Complex
+{
+	int real, img;
+
+	explicit
+	Complex(int re, int im = 0) :real(re), img(im) {}
+
+	Complex operator+(const Complex& x)
+	{
+		return Complex((real + x.real), (img + x.img));
+	}
+};
+/*07 - end */
+
+/*09 - =default  =delete */
+class Zoo {
+public:
+	Zoo(int il, int im) :d1(il), d2(im) {}
+	//copy
+	Zoo(const Zoo&) = delete;
+	//move
+	Zoo(Zoo&&) = default;
+	
+
+	//copy assignment
+	Zoo& operator=(const Zoo&) = default;
+
+	//moveassignment 
+	Zoo& operator=(const Zoo&&) = default;
+private:
+	int d1, d2;
+};
 
 int main()
 {
@@ -48,5 +84,34 @@ int main()
 	/*06 - initialzied list 背后是一个容器array */
 	/* refer to the elements of this array without containing them*/
 	/* https://en.cppreference.com/w/cpp/utility/initializer_list */
+	vector<int> v1 {2, 5, 7, 9, 11, 13, 17, 19};
+	vector<int> v2({ 10,30,40,50,60,70 });
+	vector<int> v3;
+	v3 = { 1,2,3,4,5 };
+	v3.insert(v3.begin() + 2, { 0,1,2,3,4 });
+	for (auto i : v3)
+		cout << i << ' ';
+	cout << endl;
+	cout << max({ string("Ace"),string("Stratege"),string("Sabmar"),string("Barkly"),string("Black") })<<endl;
+	cout << min({ string("Ace"),string("Stratege"),string("Sabmar"),string("Barkly"),string("Black") })<<endl;	 
+	cout << max({ 54,16,28,99,5 })<<endl;
+	cout << min({ 54,16,28,99,5 })<<endl;
+	/*07 - explicit for ctors taking more than one argu*/
+	/* one arument non explicti const before */
+	Complex c1(12, 6);
+	//Complex c2 =  c1 + 5;
+
+	/*08 - for */
+	for (auto ele : v3) {
+		cout << ele << ' ';
+	}
+	cout << endl;
+
+	for (auto& ele : v3) {
+		ele *= 3;
+		cout << ele << ' ';
+	}
+	cout << endl;
+	/*09 - BigThree =default  =delete */
 
 }
